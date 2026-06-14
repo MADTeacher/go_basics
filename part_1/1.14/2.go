695fa7fcb876ed975634ffb52732d98037666606
Stanislav Chernyshev <madmasm@gmail.com>
2026-06-14 15:02:56 +0300

    Update 2.go

--- part_1/1.14/2.go
+++ part_1/1.14/2.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var cityName string
 	var age int
-	fmt.Println("Введите название города, в котором проживаете и его возраст: ")
+	fmt.Println("Введите название города, в котором проживаете, и его возраст: ")
 	fmt.Fscan(os.Stdin, &cityName, &age)
 
 	fmt.Println(cityName, age)
