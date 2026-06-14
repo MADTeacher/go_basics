aefa0686de54aea38e716e9ab1c0cd9357935c18
Stanislav Chernyshev <madmasm@gmail.com>
2026-06-14 16:19:59 +0300

    Update server.go

--- part_8/8.4/tcp-chat/server/server.go
+++ part_8/8.4/tcp-chat/server/server.go
@@ -1,85 +1,92 @@
 package server
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"net"
+    "bufio"
+    "fmt"
+    "log"
+    "net"
+    "sync"
 )
 
 var connections []net.Conn
+var mutex sync.Mutex
 
 func checkErr(err error) {
-	if err != nil {
-		log.Print(err)
-	}
+    if err != nil {
+        log.Print(err)
+    }
 }
 
 func handleConnection(connection net.Conn) {
-	connections = append(connections, connection)
-	userName, _ := bufio.NewReader(connection).ReadString('\n')
-	userName = userName[:len(userName)-2]
-	_, err := connection.Write([]byte("Hi " + userName + "\n"))
-	checkErr(err)
+    mutex.Lock()
+    connections = append(connections, connection)
+    mutex.Unlock()
+    userName, _ := bufio.NewReader(connection).ReadString('\n')
+    userName = userName[:len(userName)-2]
+    _, err := connection.Write([]byte("Hi " + userName + "\n"))
+    checkErr(err)
 
-	for {
-		text, err := bufio.NewReader(connection).ReadString('\n')
-		if err != nil {
-			connection.Close()
-			removeConnection(connection)
-			broadCastMessage(userName+" is offline\n", connection)
-			break
-		}
+    for {
+        text, err := bufio.NewReader(connection).ReadString('\n')
+        if err != nil {
+            connection.Close()
+            removeConnection(connection)
+            broadCastMessage(userName+" is offline\n", connection)
+            break
+        }
 
-		broadCastMessage(userName+":"+text, connection)
-	}
+        broadCastMessage(userName+":"+text, connection)
+    }
 }
 
 func removeConnection(connection net.Conn) {
-	var i int
+    mutex.Lock()
+    defer mutex.Unlock()
+    var i int
 
-	for i = range connections {
-		if connections[i] == connection {
-			break
-		}
-	}
+    for i = range connections {
+        if connections[i] == connection {
+            break
+        }
+    }
 
-	if len(connections) > 1 {
-		connections = append(connections[:i], connections[i+1:]...)
-	} else {
-		connections = nil
-	}
+    if len(connections) > 1 {
+        connections = append(connections[:i], connections[i+1:]...)
+    } else {
+        connections = nil
+    }
 }
 
 func broadCastMessage(msg string, connection net.Conn) {
-	// отправка сообщения всем клиентам
-	for _, c := range connections {
-		if connection != c {
-			_, err := c.Write([]byte(msg))
-			checkErr(err)
-		}
-	}
+    // отправка сообщения всем клиентам
+    for _, c := range connections {
+        if connection != c {
+            _, err := c.Write([]byte(msg))
+            checkErr(err)
+        }
+    }
 
-	msg = msg[:len(msg)-1]
-	fmt.Println(msg)
+    msg = msg[:len(msg)-1]
+    fmt.Println(msg)
 }
 
 func RunServer(port int) {
-	// net.Listen возвращает Listener переменную,
-	// которая является общим сетевым прослушивателем для потоковых протоколов
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	checkErr(err)
+    // net.Listen возвращает Listener переменную,
+    // которая является общим сетевым прослушивателем для потоковых протоколов
+    listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+    checkErr(err)
 
-	defer listener.Close()
+    defer listener.Close()
 
-	for {
-		connection, err := listener.Accept() // ожидание подключения клиента к серверу
-		// Только после успешного вызова Accept()TCP-сервер может начать
-		// взаимодействовать с TCP-клиентами
-		if err != nil {
-			log.Print(err)
-			return
-		}
-		go handleConnection(connection)
-	}
+    for {
+        // ожидание подключения клиента к серверу
+        connection, err := listener.Accept()
+        // Только после успешного вызова Accept()TCP-сервер
+        // может начать взаимодействовать с TCP-клиентами
+        if err != nil {
+            log.Print(err)
+            return
+        }
+        go handleConnection(connection)
+    }
 }
