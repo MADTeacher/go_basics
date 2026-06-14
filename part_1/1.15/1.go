d5f8a418736588df1fc56c812bfa26c03f9cd528
Stanislav Chernyshev <madmasm@gmail.com>
2026-06-14 15:07:15 +0300

    Update 1.go

--- part_1/1.15/1.go
+++ part_1/1.15/1.go
@@ -8,24 +8,24 @@ import (
 func main() {
 	str := " !my string! "
 
-	// удяляем пробелы с обеих сторон
+	// удаляем пробелы с обеих сторон
 	fmt.Println(strings.TrimSpace(str)) // !my string!
 
-	// удяляем заданные символы в начале и в конце строки
+	// удаляем заданные символы в начале и в конце строки
 	fmt.Println(strings.Trim(str, " "))  // !my string!
 	fmt.Println(strings.Trim(str, " !")) // my string
 
-	// удяляем заданные символы в начале строки
+	// удаляем заданные символы в начале строки
 	fmt.Println(strings.TrimLeft(str, " "))   // !my string!
 	fmt.Println(strings.TrimLeft(str, " m!")) // y string!
 
-	// удяляем заданные символы в конце строки
+	// удаляем заданные символы в конце строки
 	fmt.Println(strings.TrimRight(str, " "))    // !my string!
 	fmt.Println(strings.TrimRight(str, "n!g ")) //  !my stri
 
-	// удяляем заданный символ или подстроку в начале строки
+	// удаляем заданный символ или подстроку в начале строки
 	fmt.Println(strings.TrimPrefix(str, " !my")) // string!
 
-	// удяляем заданный символ или подстроку в конце строки
+	// удаляем заданный символ или подстроку в конце строки
 	fmt.Println(strings.TrimSuffix(str, "g! ")) // !my strin
 }
